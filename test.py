5492d090984a6e4b5669bb681d811bc91b9bb91d
eushko <eushko@pikemedia.ru>
2022-11-22 23:05:29 +0300

    убрала лишнюю фикстуру

--- test.py
+++ test.py
@@ -3,5 +3,12 @@ from selene import be, have, command
 
 
 def test_some(open_browser):
-    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
+
+def test_crush(open_browser):
+    browser.element('[name="q"]').should(be.blank).type('ctkty').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
 
